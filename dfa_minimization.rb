c3a4f8ce5e9f32e45661f2ae29d643345bf6ae4f
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-09-19 11:26:30 +0900

    add debug code to compare splitting and merging algorithms

--- dfa_minimization.rb
+++ dfa_minimization.rb
@@ -48,6 +48,41 @@ class UnionFind
   end
 end
 
+def mergeable(dest_i, dest_j, h2a, uf)
+  # both action & next state must be identical
+  di = dest_i.map {|x| [h2a.call(x), uf.root(x)] }.sort
+  dj = dest_j.map {|x| [h2a.call(x), uf.root(x)] }.sort
+  di == dj
+end
+
+def construct_AS_graph(g, h2a)
+  uf = UnionFind.new(g.n)  # grouping
+
+  updated = true
+  while(updated)
+    updated = false
+    uf.roots.combination(2).each do |i,j|
+      if mergeable(g.links[i], g.links[j], h2a, uf)
+        # $stderr.puts "merging #{i} #{j}"
+        uf.merge(i, j)
+        updated = true
+        break
+      end
+    end
+  end
+
+  g2 = DirectedGraph.new(g.n)
+  g.for_each_link do |i,j|
+    ri = uf.root(i)
+    rj = uf.root(j)
+    g2.add_link(ri,rj)
+  end
+  g2.remove_duplicated_links!
+
+  return uf, g2
+end
+
+
 def equivalent(dest_i, dest_j, h2a, uf)
   # both action & next state must be identical
   di = dest_i.map {|x| [h2a.call(x), uf.root(x)] }.sort
@@ -120,6 +155,19 @@ ata_g = str.transition_graph
 #   io.puts str.transition_graph.to_dot(remove_isolated: true)
 # end
 uf, as_g = minimize_DFA( ata_g, histo_to_action )
+if DEBUG
+  $stderr.print "comparing two methods : "
+  uf2, as_g2 = construct_AS_graph( ata_g, histo_to_action )
+  if uf.to_h == uf2.to_h
+    $stderr.puts "OK"
+  else
+    $stderr.puts "NG"
+    $stderr.puts uf.inspect, uf2.inspect
+    raise "inconsistency is found"
+  end
+  exit
+end
+
 $stderr.puts uf.tree.inspect if DEBUG
 #puts "#{s} #{uf.tree.size} #{s.count('c')}"
 #exit
