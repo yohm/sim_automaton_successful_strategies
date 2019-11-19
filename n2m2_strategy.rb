484f8513f9e8b5b91315936e4be528bd62f94c18
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-11-19 11:01:53 +0900

    implemented test & defensible? method

--- n2m2_strategy.rb
+++ n2m2_strategy.rb
@@ -247,27 +247,109 @@ class Strategy
   end
 
   def defensible?
-    g = weighted_transition_graph
-    !(g.has_negative_cycle?)
-  end
-
-  def weighted_transition_graph
-    g = DirectedWeightedGraph.new(16)
+    m = Array.new(16) { Array.new(16, Float::INFINITY) }
     16.times do |i|
       s = State.make_from_id(i)
       ns = possible_next_states(s)
       ns.each do |n|
         j = n.to_id
-        g.add_link(i,j,n.relative_payoff)
+        m[i][j] = n.relative_payoff
       end
     end
-    g
+    16.times do |k|
+      16.times do |i|
+        16.times do |j|
+          if m[i][j] > m[i][k] + m[k][j]
+            m[i][j] = m[i][k] + m[k][j]
+          end
+        end
+        return false if m[i][i] < 0
+      end
+    end
+    true
   end
 end
 
 end
 
-if __FILE__ == $0 and ARGV.size > 0
+if __FILE__ == $0 and ARGV.size == 0
+  require 'minitest/autorun'
+
+  class StateTest < Minitest::Test
+
+    include N2M2
+
+    def test_all
+      assert_equal State::ALL_STATES.length, 16
+    end
+
+    def test_alld
+      s = State.make_from_id(15)
+      assert_equal [:d,:d,:d,:d], s.to_a
+      assert_equal 0, s.relative_payoff
+    end
+
+    def test_allc
+      s = State.make_from_id(0)
+      assert_equal [:c,:c,:c,:c], s.to_a
+      assert_equal 0, s.relative_payoff
+      assert_equal State.make_from_str("cccc"), s
+    end
+
+    def test_state9
+      s = State.make_from_id(9)
+      assert_equal State.make_from_str("dccd"), s
+      assert_equal -1, s.relative_payoff
+    end
+  end
+
+  class StrategyTest < Minitest::Test
+
+    include N2M2
+
+    def test_allD
+      s = Strategy.make_from_str("d"*16)
+      assert_equal 'd'*16, s.to_s
+      assert_equal :d, s.action(0)
+      assert_equal 'cdcd', s.next_state_with_self( State.make_from_str('cccc') ).to_s
+      assert_equal true, s.defensible?
+    end
+
+    def test_allC
+      s = Strategy.make_from_str("c"*16)
+      assert_equal 'c'*16, s.to_s
+      assert_equal :c, s.action(15)
+      assert_equal 'dccc', s.next_state_with_self( State.make_from_str('cdcc') ).to_s
+      assert_equal false, s.defensible?
+    end
+
+    def test_TFT
+      s = Strategy.make_from_str("cd"*8)
+      assert_equal :c, s.action('cdcc')
+      assert_equal :d, s.action('cccd')
+      assert_equal 'dccd', s.next_state_with_self( State.make_from_str('cdcc') ).to_s
+      assert_equal true, s.defensible?
+    end
+
+    def test_WSLS
+      s = Strategy.make_from_str("cdcddcdccdcddcdc")
+      assert_equal :d, s.action('cdcc')
+      assert_equal :d, s.action('cccd')
+      assert_equal :c, s.action('dddd')
+      assert_equal 'ddcd', s.next_state_with_self( State.make_from_str('cdcc') ).to_s
+      assert_equal false, s.defensible?
+    end
+
+    def test_TFT_ATFT
+      s = Strategy.make_from_str("cdcddccdcdccdccd")
+      assert_equal :d, s.action('cdcc')
+      assert_equal :d, s.action('cccd')
+      assert_equal :d, s.action('dddd')
+      assert_equal 'ddcd', s.next_state_with_self( State.make_from_str('cdcc') ).to_s
+      assert_equal true, s.defensible?
+    end
+  end
+elsif __FILE__ == $0 and ARGV.size > 0
   ARGV.each do |s|
     pp N2M2::Strategy.make_from_str(s)
   end
