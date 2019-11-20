8f75bcc10fa306cda983b3dbec4cc388b39e3fcd
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-11-20 16:12:42 +0900

    bugfix

--- simplify_transition_graph.rb
+++ simplify_transition_graph.rb
@@ -15,12 +15,15 @@ s = ARGV[0]
 if s.length == 16
   $stderr.puts "loading n=2,m=2 strategy: #{s}" if DEBUG
   str = N2M2::Strategy.make_from_bits(s)
+  to_last_actions = lambda {|i| _s = N2M2::State.make_from_id(i); [_s.a_1,_s.b_1].join }
 elsif s.length == 40
   $stderr.puts "loading n=3,m=2 strategy: #{s}" if DEBUG
   str = N3M2::Strategy.make_from_bits(s)
+  to_last_actions = lambda {|i| _s = N3M2::State.make_from_id(i); [_s.a_1,_s.b_1,_s.c_1].join }
 elsif s.length == 512
   $stderr.puts "loading n=3,m=3 strategy: #{s}" if DEBUG
   str = N3M3::Strategy.make_from_bits(s)
+  to_last_actions = lambda {|i| _s = N3M3::State.make_from_id(i); [_s.a_1,_s.b_1,_s.c_1].join }
 else
   $stderr.puts "unsupported input format"
   raise "invalid argument"
@@ -38,7 +41,7 @@ end
 link_label = Hash.new {|h,k| h[k] = [] }
 org_g.for_each_link do |i,j|
   e = [uf.root(i), uf.root(j)]
-  link_label[e].push( str.action(j) )
+  link_label[e].push( to_last_actions.call(j) )
 end
 link_label = link_label.map {|k,v| [k, v.sort.uniq.join(',')] }.to_h
 
